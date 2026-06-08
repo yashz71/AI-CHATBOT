b3363b042a3ebc8da55a9eb33fcbe32f5e8bd6d5
yassine zaoui <zaouiy59@gmail.com>
2026-06-08 19:06:35 +0100

    updates for deployment

--- main.py
+++ main.py
@@ -226,4 +226,8 @@ def download_file(filename: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8000))
+    )
